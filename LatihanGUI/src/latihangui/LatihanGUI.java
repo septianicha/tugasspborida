3c9e9bcffb44e72fbce54bee0ecd4c40f3b2a652
septianicha <septiandapuja@gmail.com>
2021-02-03 11:03:29 +0700

    tugasspborida

---
+++ LatihanGUI/src/latihangui/LatihanGUI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihangui;
+
+/**
+ *
+ * @author acer
+ */
+public class LatihanGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
